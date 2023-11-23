b987485c907ce84f2fd2cbf4e8a86c6ebecabeb3
Luca <62751046+dimeluca@users.noreply.github.com>
2023-11-23 21:37:51 +0100

    Version 1.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var sap_note_url = "https://me.sap.com/notes/"
-var sap_note_pdf_url_1 = "https://me.sap.com/servicessupport/search/%7B%22q%22%3A%22"
-var sap_note_pdf_url_2 = "%22%2C%22originContext%22%3A%22CentralSearch%22%7D"
+var sap_note_desc_url_1 = "https://me.sap.com/servicessupport/search/%7B%22q%22%3A%22"
+var sap_note_desc_url_2 = "%22%2C%22originContext%22%3A%22CentralSearch%22%7D"
 document.getElementById("searchbt1").addEventListener("click", displayNote);
 document.getElementById("searchbt2").addEventListener("click", searchNote);
 function displayNote(){
@@ -15,7 +15,7 @@ function searchNote(){
 	if(document.getElementById('form2').value.length == 0){
 		alert("Insert a Valid Description");
 	}else{
-		var final_url = sap_note_pdf_url_1 + document.getElementById('form2').value + sap_note_pdf_url_2
+		var final_url = sap_note_desc_url_1 + document.getElementById('form2').value + sap_note_desc_url_2
 		window.open(final_url, '_blank').focus();
 	}
 }
